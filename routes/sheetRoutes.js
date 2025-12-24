5af18e0737dde1ea808b69fc2c7416188362f349
Devesh Yadav <thisisdkyadav@gmail.com>
2025-12-24 21:36:02 +0530

    feat: add routes for hostel sheet data and allocation summary

--- routes/sheetRoutes.js
+++ routes/sheetRoutes.js
@@ -5,8 +5,8 @@ import { authorizeRoles } from "../middlewares/authorize.js"
 
 const router = express.Router()
 
-// router.use(authenticate)
-// router.use(authorizeRoles(["Admin", "Super Admin", "Warden", "Associate Warden"]))
+router.use(authenticate)
+router.use(authorizeRoles(["Admin", "Super Admin", "Warden", "Associate Warden"]))
 
 // Get hostel sheet data for spreadsheet view
 router.get("/hostel/:hostelId", getHostelSheetData)
